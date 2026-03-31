cca5d87aa39d45ae56c636be3ea9b6e6fabbdac1
michel_32 <karas.benjamin@gmail.com>
2026-03-31 18:33:03 +0200

    determine_type() implementation in progress for int, double and float

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: michel_32 <michel_32@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/31 14:26:37 by michel_32         #+#    #+#             */
-/*   Updated: 2026/03/31 15:52:57 by michel_32        ###   ########.fr       */
+/*   Updated: 2026/03/31 18:30:43 by michel_32        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,13 +38,91 @@ ScalarConverter& ScalarConverter::operator=(const ScalarConverter& copy)
     return (*this);
 }
 
+static bool input_is_an_int(const std::string &input, int *start)
+{
+    if (*start == input.length())
+        return (false);
+    if (input.find_first_not_of("0123456789", *start) == std::string::npos)
+        return (true);
+    return (false);
+}
+
+static bool input_is_a_double(const std::string &input, int *start, int *point_index)
+{
+    // int point_index;
+    // int start = 0;
+
+    // if (input[start] == '+' || input[start] == '-')
+    //     start++;
+
+    // if (input.find_first_of('.') == std::string::npos) //if no '.'
+    //     return (false);
+    // if (input.find_first_of('.') != input.find_last_of('.')) //if at least 2 '.'
+    //     return (false);
+    // if (input.find_first_of('.') == input.find_last_of('.')) //if only one '.'
+    //     point_index = input.find_first_of('.');
+    // if (point_index == 0 || point_index == input.size() - 1) //if '.' at the beginning or at the end
+    //     return (false);
+    if (input.find_first_not_of("0123456789", *start) == *point_index)
+        *start = *point_index + 1;
+    else
+        return (false);
+    if (input.find_first_not_of("0123456789", *start) == std::string::npos)
+        return (true);
+    else
+        return (false);
+}
+
+static bool input_is_a_float(const std::string &input, int *start, int *point_index)
+{
+    if (input.find_first_not_of("0123456789", *start) == *point_index)
+        *start = *point_index + 1;
+    else
+        return (false);
+    //need to do the same checks than the '.' for the 'f': if no 'f', if at least two 'f' etc ? 
+}
+
+//could be renamed 'input_has_a_good_point'
+static bool input_is_a_double_or_float(const std::string &input, int *point_index)
+{
+    if (input.find_first_of('.') == std::string::npos) //if there's no '.' 
+        return (false);
+    if (input.find_first_of('.') != input.find_last_of('.')) //if at least 2 '.'
+        return (false);
+    if (input.find_first_of('.') == input.find_last_of('.')) //if only one '.'
+        *point_index = input.find_first_of('.');
+    if (*point_index == 0 || *point_index == input.size() - 1) //if '.' at the beginning or at the end
+        return (false);
+    return (true);
+}
+
+
+
 static e_type determine_type(const std::string &input)
 {
+    int point_index = 0;
+    int start = 0;
+
+
+    if (input.empty())
+        return (INVALID);
     if (!input.compare("-inff") || !input.compare("inff") || !input.compare("-inf")
         || !input.compare("+inf") || !input.compare("nanf") || !input.compare("nan"))
         return (SPECIAL);
     if (input.size() == 1 && !std::isdigit(input[0]))
         return (CHAR);
+    if (input[start] == '+' || input[start] == '-')
+        start++;
+    if (input_is_an_int(input, &start))
+        return (INT);
+    if (input_is_a_double_or_float(input, &point_index))
+    {
+        if (input_is_a_double(input, &start, &point_index))
+            return (DOUBLE);
+        if (input_is_a_float(input, &start, &point_index))
+            return (FLOAT);
+    }
+    return (INVALID);
 }
 
 void  ScalarConverter::convert(const std::string &input)
